2d362ff51e508b139696791aaf1c32f088003ddb
halilyilmaaz <haliilylmaaz@gmail.com>
2022-05-12 10:20:51 +0300

    Initial Commit

---
+++ NewsLetter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsLetter
+//
+//  Created by halil yılmaz on 12.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
